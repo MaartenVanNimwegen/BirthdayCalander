b1463e5a6bfcffba280159899795659b744ca4af
MaartenVanNimwegen <89460431+MaartenVanNimwegen@users.noreply.github.com>
2023-02-04 18:47:11 +0100

    WIP - start on creating functions to save data in database.

---
+++ DataFunctions.cs
@@ -0,0 +1,19 @@
+﻿using MySql.Data.MySqlClient;
+using System.Diagnostics;
+
+namespace BirthdayCalander
+{
+    public class DataFunctions
+    {
+        public void SaveBirthday(Birthday birthday) 
+        {
+            var SaveNewBirthdayQuery = "";
+            MySqlConnection connection = new MySqlConnection { ConnectionString = DatabaseSettings.Connectionstring};
+            MySqlCommand command = new MySqlCommand(SaveNewBirthdayQuery, connection);
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+            
+        }  
+    }
+}
---
+++ DatabaseSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace BirthdayCalander
+{
+    internal class DatabaseSettings
+    {
+        public static string Connectionstring = "Data Source = localhost; Initial Catalog = quizapplicatie; User ID = root; Password = ";
+    }
+}
